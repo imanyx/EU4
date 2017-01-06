43acffa37a16f0b446255360679279f890118a58
Nyx <imanyx@tutanota.com>
2017-01-06 06:29:47 -0200

    Update akira.py

--- akira.py
+++ akira.py
@@ -7,18 +7,15 @@ class Province():
         self.provid = provid
         self.name = name
         self.dev = dev
-
+    # NOTE: Had to use __str__ to print the provinces, will change later
     def __str__(self):
         return(str(self.provid) +" "+ str(self.name) +" "+ str(self.dev))
-
 
 with open('data.csv', newline='') as f:
     reader = csv.reader(f)
     for row in reader:
+        # NOTE: .strip was very useful here, so I don't end up with brackets in my list. YAY
+        development = list([row[2].strip('('), row[3], row[4].strip(')')])
         province_id = row[0]
         province_name = str('"'+row[1]+'"')
-        # NOTE: .strip was very useful here, so I don't end up with brackets
-        # NOTE: in my list. YAY
-        development = list([row[2].strip('('), row[3], row[4].strip(')')])
-
         print(Province(province_id, province_name, development))
